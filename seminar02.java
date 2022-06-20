fd9e2807f8ce49f79ef6537c3819d4cdd9ba80ba
hott22 <mif210210@gmail.com>
2022-06-20 12:47:56 +0300

    add sem02

---
+++ seminar02.java
@@ -0,0 +1,22 @@
+public class seminar02 {
+    public static void main(String[] args) {
+//        int start_peg = 1, destination_peg = 2, buffer_peg = 3, plate_quantity = 3;
+        hanoiTowers(2 , 1, 2, 3);
+
+
+    }
+    public static void hanoiTowers(int quantity, int from, int to, int buf_peg)	{
+        if (quantity != 0)
+
+        {
+
+            hanoiTowers(quantity-1, from, buf_peg, to);
+
+            System.out.println("" + from + " -> " + to );
+
+            hanoiTowers(quantity-1, buf_peg, to, from);
+
+        }
+
+    }
+}
